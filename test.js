c2c6ebcabaf35cb1126781c529d135bb15bf865f
Slobodan Djordjevic <boba_dj1987@hotmail.com>
2017-11-23 10:51:41 +0100

    Update test.js
    Test update

--- test.js
+++ test.js
@@ -1,8 +1,14 @@
 var encrypted = require('./index.js');
+var pharse = 'You only need one Boba to rule them all';
+var encryptedPhrase;
 
 var assert = require('assert');
 describe('ROT13', function() {
-  it('Should match Slobodan Djordjevic after decrypted', function() {
-    assert.equal('Slobodan Djordjevic', encrypted.encrypt(encrypted.encrypt('Slobodan Djordjevic')));
+  it('Should not match ' + pharse + ' after encrypted', function() {
+    encryptedPhrase = encrypted.encrypt(pharse);
+  });
+  
+  it('Should match ' + pharse + ' after encrypted', function() {
+    encrypted.encrypt(encryptedPhrase);
   });
 });
